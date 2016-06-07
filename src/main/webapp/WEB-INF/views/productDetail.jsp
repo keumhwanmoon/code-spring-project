86a2c5178b7ab76825295e72dfccdf7bcec4aefa
unknown <>
2016-06-07 21:13:06 +0900

    초기 생성

---
+++ src/main/webapp/WEB-INF/views/productDetail.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Jason
+  Date: 2016-06-07
+  Time: 오후 8:37
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <span>${productVO.name}</span>
+    <span>${productVO.price}</span>
+</body>
+</html>
---
+++ src/test/java/jason/code/spring/MySQLConnectionTest.java
@@ -0,0 +1,30 @@
+package jason.code.spring;
+
+import static org.junit.Assert.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import org.junit.Test;
+
+public class MySQLConnectionTest {
+
+	private static final String DRIVER = "com.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://127.0.0.1:3306/code-spring-project?serverTimezone=UTC";
+	private static final String USER = "jason";
+	private static final String PW = "jason";
+	
+	@Test
+	public void testConnection() throws Exception {
+		Class.forName(DRIVER);
+		
+		try(Connection con = DriverManager.getConnection(URL, USER, PW)) {
+			System.out.println(con);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		fail("Not yet implemented");
+	}
+
+}
