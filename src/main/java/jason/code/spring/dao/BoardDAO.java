ba848c3082098db34f02815b03ec78993394b1b5
Jason <hwanth85@gmail.com>
2016-06-12 22:34:27 +0900

    Board 추가.

---
+++ src/main/java/jason/code/spring/dao/BoardDAO.java
@@ -0,0 +1,16 @@
+package jason.code.spring.dao;
+
+import jason.code.spring.domain.BoardVO;
+
+import java.util.List;
+
+/**
+ * Created by Jason on 2016-06-12.
+ */
+public interface BoardDAO {
+    public void create(BoardVO vo ) throws Exception;
+    public BoardVO read(Integer bno) throws Exception;
+    public void update(BoardVO vo) throws Exception;
+    public void delete(Integer bno) throws Exception;
+    public List<BoardVO> listAll() throws Exception;
+}
---
+++ src/main/java/jason/code/spring/dao/impl/BoardDAOImpl.java
@@ -0,0 +1,45 @@
+package jason.code.spring.dao.impl;
+
+import jason.code.spring.dao.BoardDAO;
+import jason.code.spring.domain.BoardVO;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import javax.inject.Inject;
+import java.util.List;
+
+/**
+ * Created by Jason on 2016-06-12.
+ */
+@Repository
+public class BoardDAOImpl implements BoardDAO {
+    @Inject
+    private SqlSession session;
+
+    private static String namespace = "jason.code.spring.mapper.BoardMapper";
+
+    @Override
+    public void create(BoardVO vo) throws Exception {
+        session.insert(namespace + ".create", vo);
+    }
+
+    @Override
+    public BoardVO read(Integer bno) throws Exception {
+        return session.selectOne(namespace + ".read", bno);
+    }
+
+    @Override
+    public void update(BoardVO vo) throws Exception {
+        session.update(namespace + ".update", vo);
+    }
+
+    @Override
+    public void delete(Integer bno) throws Exception {
+        session.delete(namespace + ".delete", bno);
+    }
+
+    @Override
+    public List<BoardVO> listAll() throws Exception {
+        return session.selectList(namespace + ".listAll");
+    }
+}
